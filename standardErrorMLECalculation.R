2e11cc4346a0cb7ddc7a66178db49eee78a8bdc2
relativelyquiet <relativelyquiet@duck.com>
2023-11-06 15:15:13 +1100

    Feat: standardErrorMELCalc - optimisation of function for one observation working.

--- standardErrorMLECalculation.R
+++ standardErrorMLECalculation.R
@@ -7,36 +7,48 @@ gamma_c_mu1<-w*rgamma(n,shape=a+b,scale=1/mu2)
 
 #Implementating whittaker function
 whittackerFunction <- function(l,m,p) {
-  subWhittackerFunction <- function(t) {
-    ((t^(m-l-0.5))*((1+t)^(m+l-0.5)))*exp(-p*t)
-  }
-  (((p^(m+.5))*exp(-p/2))/(gamma(m-l+0.5))) * integrate(subWhittackerFunction,0,Inf)$value
+  (((p^(m+.5))*exp(-p/2))/(gamma(m-l+0.5))) * integrate(subWhittackerFunction,lower=0,upper=Inf)$value
+}
+
+subWhittackerFunction <- function(t) {
+  ((t^(m-l-0.5))*((1+t)^(m+l-0.5)))*exp(-p*t)
 }
 
 #Full PDF with 4 parameters for the 2 covariates
-bivGammaPDF <- function(y1,y2,m1,m2,a,b) {
+bivGammaPDF <- function(y1,y2,mu1,mu2,a,b) {
     
   C = 1 / ( ((mu1*mu2)^(a+b)) * gamma(a+b) * gamma(a) * gamma(b) )
-  C * gamma(b) * ((y1*y2)^(a+b-1)) * (((y1/mu1)+(y2/mu2))^(((a-1)/2)-(a+b))) * exp(-0.5*((y1/mu1)+(y2/mu2))) * whittackerFunction(l=a+((1-a)/2),m=a+b-(a/2),p=(y1/m1)+(y2/m2))
+  C * gamma(b) * ((y1*y2)^(a+b-1)) * (((y1/mu1)+(y2/mu2))^(((a-1)/2)-(a+b))) * exp(-0.5*((y1/mu1)+(y2/mu2))) * whittackerFunction(l=a+((1-a)/2),m=a+b-(a/2),p=(y1/mu1)+(y2/mu2))
   
 }
 
 
+#Full PDF with 4 parameters for the 2 covariates - returns PDF
+bivGammaPDF <- function(par,y1,y2) {
+  
+  mu1 = par[1]
+  mu2 = par[2]
+  a = par[3]
+  b = par[4]
+    
+  C = 1 / ( ((mu1*mu2)^(a+b)) * gamma(a+b) * gamma(a) * gamma(b) )
+  #Returning lo
+  -log(C * gamma(b) * ((y1*y2)^(a+b-1)) * (((y1/mu1)+(y2/mu2))^(((a-1)/2)-(a+b))) * exp(-0.5*((y1/mu1)+(y2/mu2))) * whittackerFunction(l=a+((1-a)/2),m=a+b-(a/2),p=(y1/mu1)+(y2/mu2)))
+  
+}
+
+par=c(mu1,mu2,a,b)
 #Test it works (at least values are between 0 and 1) 
-bivGammaPDF(1,1,1,1,1,1)
+bivGammaPDF(par,1,1)
 
-#Alternative parameterisation
-#m1=1;m2=2;a=.25;b=1.25;
-#C = 1 / ( ((mu1*mu2)^(a+b)) * gamma(a+b) * gamma(a) * gamma(b) );
-#bivGammaPDFy1y2 <- function (y1,y2) {
-#  C * gamma(b) * ((y1*y2)^(a+b-1)) * (((y1/mu1)+(y2/mu2))^(((a-1)/2)-(a+b))) * exp(-0.5*((y1/mu1)+(y2/mu2))) * whittackerFunction(l=a+((1-a)/2),m=a+b-(a/2),p=(y1/m1)+(y2/m2)) 
+#maximiserForPDF <- function(mu1,mu2,a,b) {
+#  pdfEstimates <- vector()
+#  for (i in 1:length(gamma_c_mu1)) {
+#    y1=gamma_c_mu1[i]; y2=gamma_c_mu2[i]; pdfEstimates[i] <- bivGammaPDF(y1,y2,mu1,mu2,a,b);
+#  }
+#  sum(-2*log(pdfEstimates))
 #}
-#bivGammaPDFy1y2(1,2)
-
-for i in 1:length(gamma_c_mu1)
 
 
+optim(par=c(1,1,1,1), fn=bivGammaPDF,y1=gamma_c_mu1[1], y2=gamma_c_mu2[1], control = c(maxit=10000,fnscale=-1))
 
-f <- function (x, a) (x - a)^2
-xmin <- optimize(f, c(0, 1), tol = 0.0001, a = 1/3)
-xmin
