8e4d950437760cb3368cdbb53e85111b58497013
ahibbert <a.d.hibbert@gmail.com>
2024-03-18 10:32:38 +1100

    Add multi-plot feature. Add charts with all 3 models in one.

--- Simulation_CombineResults.R
+++ Simulation_CombineResults.R
@@ -1,10 +1,17 @@
 ###########CHOOSE DISTRIBUTION AND PARAMETERS######################################
 
-#load("results_combined_nointGA_1000_2024-03-15.RData"); dist="GA" ####GA
+load("results_combined_nointGA_1000_2024-03-15.RData"); dist="GA" ####GA
 #load("results_combined_nointNO_1000_2024-03-15.RData"); dist="NO" ####NO
-#load("results_combined_nointPO_1000_2024-03-15.RData"); dist="PO" ####NB/PO
+#load("results_combined_nointPO_1000_2024-03-16.RData"); dist="PO" ####NB/PO
+######load("results_combined_nointPO_1000_2024-03-15.RData"); dist="PO" ####NB/PO
 
-load("results_combined_nointPO_1000_2024-03-16.RData"); dist="PO" ####NB/PO
+multiplot=TRUE
+
+#################################1. DATA SETUP##################################################
+
+if ( (multiplot==FALSE) || (multiplot==TRUE && (!exists("first_run"))) ) {
+  plot_count_lik<-plotcount<-0; bias_plots <- list(); error_plots <- list(); lik_plots<-list(); first_run=FALSE;
+}
 
 check_length=c()
 for (i in 1:length(results_combined)) {
@@ -49,8 +56,6 @@ plotVersusTrue <- function (limits,inputs,true,tau,xlab,ylab,scaled=FALSE,type="
                         , values=c(brewer.pal(n = 7, name = "Dark2"),"#000000"))
   return(plot)
 }
-
-#################################1. DATA SETUP##################################################
 
 require(latex2exp)
 require(ggplot2)
@@ -98,6 +103,8 @@ if(dist=="PO") {
   #  ) 
   #)/sqrt(parameters[,"n"])    
   
+  mu1=mu2=vector()
+  
   for (i in 1:length(results)) {
     trueSE[i,]<-c(results[[i]]["actuals",c("se_b1","se_b2","LogLik")]) 
     mu1[i]=c(results[[i]]["actuals",c("b_1")]) 
@@ -124,23 +131,31 @@ for (i in 1:length(results)) {
   colnames(t1intercepts)<-colnames(t2intercepts)<-colnames(t1error)<-colnames(t2error)<-colnames(loglik)<-colnames(aic)<-colnames(bic)<-colnames(t(results[[i]][1:(nrow(results[[i]])-2),"b_1"]))
 }
 
-###################### BIAS CHARTS ######################
+###################### PLOT SETUP######################
 
 library(latex2exp)
 limits_bias = c(.1,0.7,-1,1); xlab=TeX("Kendall's \\tau")
+limits_error <- c(limits_bias[1:2],0,.1)
 #if(dist=="NO") {tau=parameters[,"c"]; xlab="Pearson Correlation"}
 
-nrow(t1intercepts)
-nrow(t2intercepts)
-
-bias_1_plot<- plotVersusTrue(limits_bias
+plotcount=plotcount+1
+bias_plots[[plotcount]]<- plotVersusTrue(limits_bias
                ,if(dist=="NO"){t1intercepts}else{exp(t1intercepts)}
                ,mu1
                ,tau
                ,xlab
                ,ylab=TeX("$(\\hat{\\mu_1}/\\mu_1)-1$")
                , scaled=TRUE)
-bias_2_plot<- plotVersusTrue(limits_bias
+error_plots[[plotcount]]<- plotVersusTrue(limits_error
+                                          ,t1error
+                                          ,trueSE[,"mu1_se"]
+                                          ,tau
+                                          ,xlab
+                                          ,ylab=TeX("$SE(\\hat{\\beta_{1}})$")
+                                          ,scaled=FALSE)
+
+plotcount=plotcount+1
+bias_plots[[plotcount]]<- plotVersusTrue(limits_bias
                ,if(dist=="NO"){cbind((t2intercepts)[,1:5],(t2intercepts)[,6:ncol(t2intercepts)])}
                 else{cbind(exp(t2intercepts)[,1:5],exp(t2intercepts)[,6:ncol(t1intercepts)])}
                ,mu2
@@ -148,159 +163,60 @@ bias_2_plot<- plotVersusTrue(limits_bias
                ,xlab
                ,ylab=TeX("$(\\hat{\\mu_2}/\\mu_2)-1$")
                ,scaled=TRUE)
-plot.new()
-  ggarrange(bias_1_plot,bias_2_plot,common.legend=TRUE,nrow=1, ncol=2, legend="right",labels="AUTO") + #,labels=c("(a)","(b)","(c)","(d)"), font.label = list(size=12,face="plain"
-    bgcolor("white")+border(color = "white")  + guides(color=guide_legend(override.aes=list(fill=NA)))
-
-#ggsave(file=paste("simulation_bias_AIO_",dist,"_",parameters[1,"n"],"_",Sys.Date(),".png",sep=""),last_plot(),width=12,height=3,dpi=900)
-  
-###################### ERROR CHARTS #####################
-
-limits_error <- c(limits_bias[1:2],0,.1)
-
-error_1_plot<- plotVersusTrue(limits_error
-                             ,t1error
-                             ,trueSE[,"mu1_se"]
-                             ,tau
-                             ,xlab
-                             ,ylab=TeX("$SE(\\hat{\\beta_{1}})$")
-                             ,scaled=FALSE)
-error_2_plot<- plotVersusTrue(limits_error
-                              ,t2error
-                              ,trueSE[,"mu2_se_B2"]
-                              ,tau
-                              ,xlab
-                              ,ylab=TeX("$SE(\\hat{\\beta_{2}})$")
-                              ,scaled=FALSE)
-
-ggarrange(error_1_plot,error_2_plot,common.legend=TRUE,nrow=1, ncol=2, legend="right",labels="AUTO") + #,labels=c("(a)","(b)","(c)","(d)"), font.label = list(size=12,face="plain"
-    bgcolor("white")+border(color = "white")
-  
-#ggsave(file=paste("simulation_error_AIO_",dist,"_",parameters[1,"n"],"_",Sys.Date(),".png",sep=""),last_plot(),width=12,height=3,dpi=900)
+error_plots[[plotcount]]<- plotVersusTrue(limits_error
+                                          ,t2error
+                                          ,trueSE[,"mu2_se_B2"]
+                                          ,tau
+                                          ,xlab
+                                          ,ylab=TeX("$SE(\\hat{\\beta_{2}})$")
+                                          ,scaled=FALSE)
 
 ########Likelihoods
-
 limits_lik <- c(limits_bias[1:2],NA,NA)
 if (dist=="PO") {limits_lik <- c(limits_bias[1:2],-5000,0)}
 
-lik1<- plotVersusTrue(limits_lik
-                              ,loglik
-                              ,NA
-                              ,tau
-                              ,xlab
-                              ,ylab="LogLik"
-                              ,scaled=FALSE
-                              ,plotTrue = FALSE)
-lik2<- plotVersusTrue(c(limits_lik[1:2],limits_lik[c(4,3)]*-2)
-                              ,aic
-                              ,NA
-                              ,tau
-                              ,xlab
-                              ,ylab="AIC"
-                              ,scaled=FALSE
-                              ,plotTrue = FALSE)
-lik3<- plotVersusTrue(c(limits_lik[1:2],limits_lik[c(4,3)]*-2)
-                                 ,bic
-                                 ,NA
-                                 ,tau
-                                 ,xlab
-                                 ,ylab="BIC"
-                                  ,scaled=FALSE
-                                 ,plotTrue = FALSE)
+plot_count_lik=plot_count_lik+1
+lik_plots[[plot_count_lik]]<- plotVersusTrue(limits_lik
+                                             ,loglik
+                                             ,NA
+                                             ,tau
+                                             ,xlab
+                                             ,ylab="LogLik"
+                                             ,scaled=FALSE
+                                             ,plotTrue = FALSE)
+plot_count_lik=plot_count_lik+1
+lik_plots[[plot_count_lik]]<- plotVersusTrue(c(limits_lik[1:2],limits_lik[c(4,3)]*-2)
+                                             ,aic
+                                             ,NA
+                                             ,tau
+                                             ,xlab
+                                             ,ylab="AIC"
+                                             ,scaled=FALSE
+                                             ,plotTrue = FALSE)
+plot_count_lik=plot_count_lik+1
+lik_plots[[plot_count_lik]]<- plotVersusTrue(c(limits_lik[1:2],limits_lik[c(4,3)]*-2)
+                                             ,bic
+                                             ,NA
+                                             ,tau
+                                             ,xlab
+                                             ,ylab="BIC"
+                                             ,scaled=FALSE
+                                             ,plotTrue = FALSE)
 
-ggarrange(lik1,lik2,lik3,common.legend=TRUE,nrow=1, ncol=3, legend="right",labels="AUTO") + #,labels=c("(a)","(b)","(c)","(d)"), font.label = list(size=12,face="plain"
+###################PLOT FUNCTION#############################
+#Bias
+plot.new()
+ggarrange(plotlist=bias_plots, common.legend = TRUE,ncol=2,nrow=3,labels="AUTO") + 
+  bgcolor("white")+border(color = "white")  + guides(color=guide_legend(override.aes=list(fill=NA)))
+ggsave(file=paste("simulation_bias_AIO_",parameters[1,"n"],"_",Sys.Date(),".png",sep=""),last_plot(),width=8,height=9,dpi=900)
+
+#Standard Error
+plot.new()
+ggarrange(plotlist=error_plots,common.legend=TRUE,nrow=3, ncol=2,labels="AUTO") + #,labels=c("(a)","(b)","(c)","(d)"), font.label = list(size=12,face="plain"
+  bgcolor("white")+border(color = "white") + guides(color=guide_legend(override.aes=list(fill=NA)))
+#ggsave(file=paste("simulation_error_AIO_",parameters[1,"n"],"_",Sys.Date(),".png",sep=""),last_plot(),width=8,height=9,dpi=900)
+
+#Likelihood
+ggarrange(plotlist=lik_plots,common.legend=TRUE,nrow=3, ncol=3,labels="AUTO") + #,labels=c("(a)","(b)","(c)","(d)"), font.label = list(size=12,face="plain"
   bgcolor("white")+border(color = "white")
-ggsave(file=paste("simulation_loglik_AIO_",dist,"_",parameters[1,"n"],"_",Sys.Date(),".png",sep=""),last_plot(),width=12,height=3,dpi=900)
-
-
-
-
-  ##########Individual plots###############
-  par(mfrow=c(2,3))
-  plot(x=trueSE[,"mu1_se"],y=t1error[,"summary_glm"]      ,xlab="True SE",ylab="Estimated SE", main="GLM")      ; abline(a=0, b=1,col="red")
-  plot(x=trueSE[,"mu1_se"],y=t1error[,"summary_gee"]      ,xlab="True SE",ylab="Estimated SE", main="GEE")      ; abline(a=0, b=1,col="red")
-  plot(x=trueSE[,"mu1_se"],y=t1error[,"summary_re_nosig"] ,xlab="True SE",ylab="Estimated SE", main="GLMM (4)") ; abline(a=0, b=1,col="red")
-  plot(x=trueSE[,"mu1_se"],y=t1error[,"summary_re_np"]       ,xlab="True SE",ylab="Estimated SE", main="GLMM (5)") ; abline(a=0, b=1,col="red")
-  plot(x=trueSE[,"mu1_se"],y=t1error[,"summary_cop"]      ,xlab="True SE",ylab="Estimated SE", main="GJRM (C)") ; abline(a=0, b=1,col="red")
-  plot(x=trueSE[,"mu1_se"],y=t1error[,"summary_cop_n"]    ,xlab="True SE",ylab="Estimated SE", main="GJRM (N)") ; abline(a=0, b=1,col="red")
-  
-  par(mfrow=c(2,3))
-  plot(x=trueSE[,"mu2_se_Bt"],y=t2error[,"summary_glm"]      ,xlab="True SE",ylab="Estimated SE", main="GLM")      ; abline(a=0, b=1,col="red")
-  plot(x=trueSE[,"mu2_se_Bt"],y=t2error[,"summary_gee"]      ,xlab="True SE",ylab="Estimated SE", main="GEE")      ; abline(a=0, b=1,col="red")
-  plot(x=trueSE[,"mu2_se_Bt"],y=t2error[,"summary_re_nosig"] ,xlab="True SE",ylab="Estimated SE", main="GLMM (4)") ; abline(a=0, b=1,col="red")
-  plot(x=trueSE[,"mu2_se_Bt"],y=t2error[,"summary_re"]       ,xlab="True SE",ylab="Estimated SE", main="GLMM (5)") ; abline(a=0, b=1,col="red")
-  plot(x=trueSE[,"mu2_se_B2"],y=t2error[,"summary_cop"]      ,xlab="True SE",ylab="Estimated SE", main="GJRM (C)") ; abline(a=0, b=1,col="red")
-  plot(x=trueSE[,"mu2_se_B2"],y=t2error[,"summary_cop_n"]    ,xlab="True SE",ylab="Estimated SE", main="GJRM (N)") ; abline(a=0, b=1,col="red")
-  
-  ##############TESTING
-  plot.new()  
-  par(mfrow=c(3,2))
-  
-  scatter.smooth(t1error[,1]~tau,main="Error GLM",ylim=c(0,.3),xlab="Tau", ylab="Error",col="gray")
-  lines(lowess(t1error[,1]~tau),col="red")
-  scatter.smooth(t1error[,2]~tau,main="Error GEE",ylim=c(0,.3),xlab="Tau", ylab="Error",col="gray")
-  lines(lowess(t1error[,1]~tau),col="red")
-  
-  scatter.smooth(t1error[,3]~tau,main="Error RE no sig",ylim=c(0,.3),xlab="Tau", ylab="Error",col="gray")
-  lines(lowess(t1error[,1]~tau),col="red")
-  scatter.smooth(t1error[t1error[,4]>.0001,4]~tau[t1error[,4]>.0001],main="Error RE",ylim=c(0,.3),xlab="Tau", ylab="Error",col="gray")
-  lines(lowess(t1error[,1]~tau),col="red")
-  
-  scatter.smooth(t1error[,5]~tau,main="Error Copula Clayton",ylim=c(0,.3),xlab="Tau", ylab="Error",col="gray")
-  lines(lowess(t1error[,1]~tau),col="red")
-  scatter.smooth(t1error[,6]~tau,main="Error Copula Normal",ylim=c(0,.3),xlab="Tau", ylab="Error",col="gray")
-  lines(lowess(t1error[,1]~tau),col="red")
-  
-  plot(t1intercepts[,1]/log(t1intercepts[,7]/mu1)-1~tau,main="Bias GLM",ylim=c(-4,4),xlab="Tau", ylab="Bias")
-  abline(h=0,col="red")
-  plot(t1intercepts[,2]/log(t1intercepts[,7]/mu1)-1~tau,main="Bias GEE",ylim=c(-4,4),xlab="Tau", ylab="Bias")
-  abline(h=0,col="red")
-  
-  plot(t1intercepts[,3]/log(t1intercepts[,7]/mu1)-1~tau,main="Bias RE no sig",ylim=c(-4,4),xlab="Tau", ylab="Bias")
-  abline(h=0,col="red")
-  plot(t1intercepts[t1error[,4]>.0001,4]/log(t1intercepts[t1error[,4]>.0001,7]/mu1)-1~tau[t1error[,4]>.0001],main="Bias RE",ylim=c(-4,4),xlab="Tau", ylab="Bias")
-  abline(h=0,col="red")
-  
-  plot(t1intercepts[,5]/log(t1intercepts[,7]/mu1)-1~tau,main="Bias Copula Clayton",ylim=c(-4,4),xlab="Tau", ylab="Bias")
-  abline(h=0,col="red")
-  plot(t1intercepts[,6]/log(t1intercepts[,7]/mu1)-1~tau,main="Bias Copula Normal",ylim=c(-4,4),xlab="Tau", ylab="Bias")
-  abline(h=0,col="red")
-  
-  ##############BOXPLOTS
-  plot.new()
-  par(mfrow=c(3,2))
-  plot(t1intercepts[,1]/log(t1intercepts[,7]/mu1)-1~as.factor(round(tau*10)/10),ylim=c(-5,5),xlab="Tau", ylab="Bias",main="Bias GLM"); abline(h=0,col="blue")
-  plot(t1intercepts[,2]/log(t1intercepts[,7]/mu1)-1~as.factor(round(tau*10)/10),ylim=c(-5,5),xlab="Tau", ylab="Bias",main="Bias GEE"); abline(h=0,col="blue")
-  plot(t1intercepts[,3]/log(t1intercepts[,7]/mu1)-1~as.factor(round(tau*10)/10),ylim=c(-5,5),xlab="Tau", ylab="Bias",main="Bias GLMM no sig"); abline(h=0,col="blue")
-  plot(t1intercepts[,4]/log(t1intercepts[,7]/mu1)-1~as.factor(round(tau*10)/10),ylim=c(-5,5),xlab="Tau", ylab="Bias",main="Bias GLMM"); abline(h=0,col="blue")
-  plot(t1intercepts[,5]/log(t1intercepts[,7]/mu1)-1~as.factor(round(tau*10)/10),ylim=c(-5,5),xlab="Tau", ylab="Bias",main="Bias GJRM (Clayton)"); abline(h=0,col="blue")
-  plot(t1intercepts[,6]/log(t1intercepts[,7]/mu1)-1~as.factor(round(tau*10)/10),ylim=c(-5,5),xlab="Tau", ylab="Bias",main="Bias GJRM (Gaussian)"); abline(h=0,col="blue")
-  
-  plot.new()
-  par(mfrow=c(3,2))
-  plot(t1error[,1]~as.factor(round(tau*10)/10),ylim=c(0,.3),xlab="Tau", ylab="Standard Error",main="Standard Error GLM"); abline(h=0,col="blue")
-  plot(t1error[,2]~as.factor(round(tau*10)/10),ylim=c(0,.3),xlab="Tau", ylab="Standard Error",main="Standard Error GEE"); abline(h=0,col="blue")
-  plot(t1error[,3]~as.factor(round(tau*10)/10),ylim=c(0,.3),xlab="Tau", ylab="Standard Error",main="Standard Error GLMM no sig"); abline(h=0,col="blue")
-  plot(t1error[t1error[,4]>0.001,4]~as.factor(round(tau[t1error[,4]>0.001]*10)/10),ylim=c(0,.3),xlab="Tau", ylab="Standard Error",main="Standard Error GLMM"); abline(h=0,col="blue")
-  plot(t1error[,5]~as.factor(round(tau*10)/10),ylim=c(0,.3),xlab="Tau", ylab="Standard Error",main="Standard Error GJRM (Clayton)"); abline(h=0,col="blue")
-  plot(t1error[,6]~as.factor(round(tau*10)/10),ylim=c(0,.3),xlab="Tau", ylab="Standard Error",main="Standard Error GJRM (Gaussian)"); abline(h=0,col="blue")
-  
-  plot.new()
-  par(mfrow=c(3,2))
-  plot(t2intercepts[,1]/log((t1intercepts[,7]/mu1)/(t1intercepts[,7]/mu2))+1~as.factor(round(tau*10)/10),main="Bias GLM",ylim=c(-4,4),xlab="Tau", ylab="Bias"); abline(h=0,col="blue")
-  plot(t2intercepts[,2]/log((t1intercepts[,7]/mu1)/(t1intercepts[,7]/mu2))+1~as.factor(round(tau*10)/10),main="Bias GEE",ylim=c(-4,4),xlab="Tau", ylab="Bias"); abline(h=0,col="blue")
-  plot(t2intercepts[,3]/log((t1intercepts[,7]/mu1)/(t1intercepts[,7]/mu2))+1~as.factor(round(tau*10)/10),main="Bias GLMM no sig",ylim=c(-4,4),xlab="Tau", ylab="Bias"); abline(h=0,col="blue")
-  plot(t2intercepts[t2error[,4]>0.001,4]/log((t1intercepts[t2error[,4]>0.001,7]/mu1)/(t1intercepts[t2error[,4]>0.001,7]/mu2))+1~as.factor(round(tau*10)/10),main="Bias GLMM",ylim=c(-4,4),xlab="Tau", ylab="Bias"); abline(h=0,col="blue")
-  plot(t2intercepts[,5]/log((t1intercepts[,7]/mu1)/(t1intercepts[,7]/mu2))+1~as.factor(round(tau*10)/10),main="Bias GJRM (Copula)",ylim=c(-4,4),xlab="Tau", ylab="Bias"); abline(h=0,col="blue")
-  plot(t2intercepts[,6]/log((t1intercepts[,7]/mu1)/(t1intercepts[,7]/mu2))+1~as.factor(round(tau*10)/10),main="Bias GJRM (Gaussian)",ylim=c(-4,4),xlab="Tau", ylab="Bias"); abline(h=0,col="blue")
-  
-  plot.new()
-  par(mfrow=c(3,2))
-  plot(t2error[,1]~as.factor(round(tau*10)/10),ylim=c(0,.3),xlab="Tau", ylab="Standard Error",main="Standard Error GLM"); abline(h=0,col="blue")
-  plot(t2error[,2]~as.factor(round(tau*10)/10),ylim=c(0,.3),xlab="Tau", ylab="Standard Error",main="Standard Error GEE"); abline(h=0,col="blue")
-  plot(t2error[,3]~as.factor(round(tau*10)/10),ylim=c(0,.3),xlab="Tau", ylab="Standard Error",main="Standard Error GLMM no sig"); abline(h=0,col="blue")
-  plot(t2error[t2error[,4]>0.001,4]~as.factor(round(tau[t2error[,4]>0.001]*10)/10),ylim=c(0,.3),xlab="Tau", ylab="Standard Error",main="Standard Error GLMM"); abline(h=0,col="blue")
-  plot(t2error[,5]~as.factor(round(tau*10)/10),ylim=c(0,.3),xlab="Tau", ylab="Standard Error",main="Standard Error GJRM (Clayton)"); abline(h=0,col="blue")
-  plot(t2error[,6]~as.factor(round(tau*10)/10),ylim=c(0,.3),xlab="Tau", ylab="Standard Error",main="Standard Error GJRM (Gaussian)"); abline(h=0,col="blue")
-  
-  
+ggsave(file=paste("simulation_loglik_AIO_",parameters[1,"n"],"_",Sys.Date(),".png",sep=""),last_plot(),width=12,height=9,dpi=900)
