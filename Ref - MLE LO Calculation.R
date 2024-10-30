9ca3a57046715a65765f209a4f38280f28e1a626
ahibbert <40676358+ahibbert@users.noreply.github.com>
2024-10-30 14:18:22 +1100

    Add function to estimate standard errors for model parameters of the logistic distribution.

--- Ref - MLE LO Calculation.R
+++ Ref - MLE LO Calculation.R
@@ -1,32 +1,28 @@
 ###numerical derivatives for the poisson
 
-poisson_dist <- function(par, n=1) {
+source("link_functions.R")
+
+LO_dist <- function(par, n=1) {
   #Compound multiple poisson of Stein & Juritz, 1987
   a=par[1];b=par[2];c=a=par[3];mu1=par[4];mu2=par[5]
   
-  mixing_dist<-rgamma(n,shape=c,scale=b)
+  require(MASS)
+  a=1;b=1
+  normData<-mvrnorm(n,mu=c(0,0),Sigma = matrix(c(a^2,c*a*b,c*a*b,b^2),nrow=2))
+  margin_1<-as.numeric(pnorm(normData[,1])<=mu1)
+  margin_2<-as.numeric(pnorm(normData[,2])<=mu2)
   
-  margin_1=vector(length = n) 
-  margin_2=vector(length = n) 
-  for (i in 1:n) {
-    margin_1[i]=rpois(1,mu1*mixing_dist[i])
-  }
-  for (i in 1:n) {
-    margin_2[i]=rpois(1,mu2*mixing_dist[i])
-  }
   return(cbind(margin_1,margin_2))
 }
-
-a=NA; b=c(.2,.5,1,2,5);c=c(.2,.5,1,2,5); mu1=c(.5,1,2,5); mu2=c(.5,1,2,5); n=1000;dist="PO"
-#a=NA; b=c(.2,.5,1,2,5);c=c(.2,.5,1,2,5); mu1=c(.5,1,2,5); mu2=c(.5,1,2,5); n=1000;dist="PO"
+a=NA; b=NA;c=c(.1,.25,.5,.75,.9); mu1=c(.1,.25,.5,.75,.9); mu2=c(.1,.25,.5,.75,.9); n=1000;dist="LO"
 
 #Code to iterate through various shapes of the bivariate distribution and fit the non-GJRM models
 i=1; j=1; k=1; l=1; z=1
 start=Sys.time()
-iterations=length(a)*length(b)*length(mu1)*length(mu2)*length(c)*2 #times 2 for types
+iterations=length(a)*length(b)*length(mu1)*length(mu2)*length(c) #times 2 for types
 dataset=list()
 means=list()
-mle=matrix(NA,ncol=2,nrow=0)
+mle=matrix(NA,ncol=6,nrow=0)
 colnames=c("mean","var")
 nb_par=matrix(NA,ncol=5,nrow=0)
 
@@ -35,21 +31,28 @@ for (i in 1:length(a)) {
     for (k in 1:length(mu1)) {
       for (l in 1:length(mu2)) {
         for (m in 1:length(c)) {
+          start=Sys.time()
           dataset[[z]]=list()
-          means[[z]]=matrix(NA,ncol=1,nrow=0)
+          means[[z]]=matrix(NA,ncol=3,nrow=0)
           for (i in 1:1000) {
-            dataset[[z]][[i]]=(poisson_dist(par=c(a[i],b[j],c[m],mu1[k],mu2[l]),n=1000))
-            means[[z]]=rbind(means[[z]],log(mean(dataset[[z]][[i]][,2])/mean(dataset[[z]][[i]][,1])))
+            dataset[[z]][[i]]=(LO_dist(par=c(a[i],b[j],c[m],mu1[k],mu2[l]),n=1000))
+            means[[z]]=rbind((means[[z]]),c(logit(mean(dataset[[z]][[i]][,2])),logit(mean(dataset[[z]][[i]][,1])),logit(mean(dataset[[z]][[i]][,1]))-logit(mean(dataset[[z]][[i]][,2]))))
           }
-          mle=rbind(mle,c(mean(means[[z]]),var(means[[z]])))
+          mle=rbind(mle,c(colMeans(means[[z]]),var(means[[z]][,1]),var(means[[z]][,2]),var(means[[z]][,3])))
           nb_par=rbind(nb_par,c(a[i],b[j],c[m],mu1[k],mu2[l]))
+          print(c(z
+                  ,z/(iterations) 
+                  , (Sys.time()-start)
+                  , (Sys.time()-start) / (z/iterations)))
+          
+          z=z+1
         }
       }
     }
   }
 }
 
-nb_mle<-cbind(nb_par,mle)
-colnames(nb_mle)=c("a","b","c","mu1","mu2","mean_Bt","var_Bt")
+lo_mle<-cbind(nb_par,mle)
+colnames(lo_mle)=c("a","b","c","mu1","mu2","mean_B1","mean_B2","mean_Bt","var_B1","var_B2","var_Bt")
 
-save(nb_mle,file="nb_mle")
+save(lo_mle,file="Data/lo_mle")
