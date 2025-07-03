674a261af2a5a2cfa53dd50dfd4c2e06db446d3c
ahibbert <40676358+ahibbert@users.noreply.github.com>
2025-07-03 16:26:55 +1000

    Working outer loop for method with covariates

--- Simulations - 1 - Generate and Fit Models (with Covariates).R
+++ Simulations - 1 - Generate and Fit Models (with Covariates).R
@@ -1,3 +1,13 @@
+# Set up a list of input configurations you wish to run
+input_list <- list(
+  list(Bt_mode=FALSE , a=1    , b=2        ,c=.75, mu1=1          , mu2=2                  , n=1000,dist="NO",x1=c(1,3),x2=1),
+  list(Bt_mode=FALSE , a=.5*1:5    , b=.5*1:5        ,c=c(.1,.2,.3,.4,.5,.6,.7,.8,.9), mu1=1          , mu2=2                  , n=1000,dist="NO",x1=1,x2=1),
+  list(Bt_mode=FALSE , a=NA        , b=c(.2,.5,1,2,5),c=c(.2,.5,1,2,5)               , mu1=c(.5,1,2,5), mu2=c(.5,1,2,5)        , n=1000,dist="PO",x1=1,x2=1),
+  list(Bt_mode=FALSE , a=.1+.1*1:20, b=.1+.1*1:20    ,c=NA                           , mu1=10         , mu2=12                 , n=1000,dist="GA",x1=1,x2=1),
+  list(Bt_mode=FALSE , a=NA        , b=NA            ,c=c(.1,.25,.5,.75,.9), mu1=c(.1,.25,.5,.75,.9)  , mu2=c(.1,.25,.5,.75,.9), n=1000,dist="LO",x1=1,x2=1)
+)
+list2env(input_list[[1]], envir = .GlobalEnv)
+
 options(scipen=999)
 source("common_functions.R")
 results<-list()
@@ -9,7 +19,7 @@ model_struct_FUN=fitBivModels_Bt_withCov
 #Code to iterate through various shapes of the bivariate distribution and fit the non-GJRM models
 i=1; j=1; k=1; l=1; z=1
 start=Sys.time()
-iterations=length(a)*length(b)*length(mu1)*length(mu2)*length(c)*2 #times 2 for types
+iterations=length(a)*length(b)*length(mu1)*length(mu2)*length(c)*length(x1)*length(x2)*2 #times 2 for types
 
 for (type in c("non-GJRM","GJRM")) {
   d=1
@@ -24,7 +34,8 @@ for (type in c("non-GJRM","GJRM")) {
                 if(type == "non-GJRM") {datasets[[d]]<-generateBivDist_withCov(n,a[i],b[j],c[m],mu1[k],mu2[l],dist,x1[o],x2[p])}
                 tryCatch( {results[[z]] <- model_struct_FUN(datasets[[d]],dist,include=type,a[i],b[j],c[m],mu1[k],mu2[l])}
                           , error = function(e) {results[[z]]<- NA})
-                print(c(n,a[i],b[j],c[m],mu1[k],mu2[l],NA,NA,type))
+                results[[z]]$actuals=c(n,a[i],b[j],c[m],mu1[k],mu2[l],x1[o],x2[p],type)
+                print(c(n,a[i],b[j],c[m],mu1[k],mu2[l],x1[o],x2[p]))
                 print(c(z
                         ,z/(iterations) 
                         , (Sys.time()-start)
@@ -45,10 +56,14 @@ na_check=rep(TRUE,length(results)/2)
 results_combined <- list()
 for(i in 1:(length(results)/2)) {
   print(i); 
-  results_combined[[i]]=tryCatch( {rbind(results[[i]][1:(nrow(results[[i]])-2),],results[[(length(results)/2)+i]])}, error = function(e) {c(NA,NA,NA,NA,NA,NA,NA,NA)} )
+  results_combined[[i]]=list()
+  for (j in 1:length(results[[i]])) {
+    results_combined[[i]][[j]]=tryCatch( {rbind(results[[i]][[j]],results[[(length(results)/2)+i]][[j]])}, error = function(e) {NA} )  
+  }
+  
   na_check[i]=(is.null(nrow(results_combined[[i]])))
 }
-results_combined <- results_combined[na_check==FALSE]
+#results_combined <- results_combined[na_check==FALSE]
 
 ##### SAVE LOCATION
 if(Bt_mode==TRUE) {
