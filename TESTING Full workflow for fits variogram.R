075393527889e042b61e5b77785ba7a22e0abc4e
ahibbert <a.d.hibbert@gmail.com>
2025-08-12 14:26:03 +1000

    Add loop to extract coefficient and SE data from iterations for plotting.

--- TESTING Full workflow for fits variogram.R
+++ TESTING Full workflow for fits variogram.R
@@ -29,18 +29,41 @@ for (i in 1:outer_sims) {
 }
 
 score_items=list()
-score_item_names=c("vs2_wt"   ,      "vs2",            "es","vs1"          ,  "vs2_wt_coronly")
+score_item_names=c("vs2_wt"   ,      "vs2",            "es","vs1"          ,  "vs2_wt_coronly","logliks")
 for (i in 1:length(eval_outer)) {
-  item_names=names(eval_outer[[i]])
+  #item_names=names(eval_outer[[i]])
   for(item in score_item_names) {
+    new_item=if(item=="logliks"){eval_outer[[i]][[item]][,1]}else{eval_outer[[i]][[item]]}
+     
     if(i==1) {
-      score_items[[item]]=eval_outer[[i]][[item]]  
+      score_items[[item]]=new_item
     } else {
-      score_items[[item]]=rbind(score_items[[item]],eval_outer[[i]][[item]])
+      score_items[[item]]=rbind(score_items[[item]],new_item)
     }
   }
 }
 
+par_estimates=list()
+par_item_names=c("coefficients","ses","sigmas","correlations")
 
+for(i in 1:11) {
+  par_estimates[[i]]=matrix(nrow=length(eval_outer),ncol=16)
+}
 
-#save(eval_outer,file="Data/eval_outer_NO_1_1_75_1_2_1_1_100.RData")
+for(i in 1:length(eval_outer)) {
+  z=1
+  for(item in par_item_names) {
+    new_item=data.frame(eval_outer[[i]][[item]])
+    for (j in 1:ncol((new_item))) {
+      par_estimates[[z]][i,]=new_item[,j]
+      z=z+1; 
+    }
+  }
+}
+names(par_estimates)=c("mu1","mu2","x1","x2","mu1_se","mu2_se","x1_se","x2_se","sigma1","sigma2","corr")
+for(item in names(par_estimates)) {
+  colnames(par_estimates[[item]])=c("glm", "gee", "re_nosig", "re_np", "lme4", "gamm",
+                                    "cop", "cop_n", "cop_j", "cop_g", "cop_f",
+                                    "cop_amh", "cop_fgm", "cop_pl", "cop_h", "cop_t")
+}
+
