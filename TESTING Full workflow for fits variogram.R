df1c7674651adb7ee4925bf6bb53bd0106e9590d
ahibbert <a.d.hibbert@gmail.com>
2025-08-20 14:04:56 +1000

    Fully automate code for model fitting workflow.

--- TESTING Full workflow for fits variogram.R
+++ TESTING Full workflow for fits variogram.R
@@ -1,95 +1,19 @@
-# source("common_functions.R")
-# set.seed(1)
-# dist="NO";a=1;b=1;c=.25;mu1=1;mu2=2;x1=1;x2=0.1;n=1000
-# #dist="GA";a=1;b=1;c=NA;mu1=1;mu2=2;x1=1;x2=.1;n=1000
-# 
-# #Manual single run
-# #dataset=generateBivDist_withCov(n=n,a=a,b=b,c=c,mu1=mu1,mu2=mu2,dist=dist,x1=x1,x2=x2)
-# #fits=fitBivModels_Bt_withCov(      dataset=dataset,      dist=dist,      include = "ALL",      a=a,b=b,c=c,mu1=mu1,mu2=mu2,      calc_actuals = FALSE,      cv = FALSE    )
-# #eval=evaluateModels(fits,vg_sims=100)
-# #eval
-# 
-# library(callr)
-# outer_sims=10
-# eval_outer=list()
-# for (i in 1:outer_sims) {
-#   print(i)
-#   eval <- callr::r(
-#     func = function(dist,a,b,c,mu1,mu2,x1,x2,n) {
-#       source("common_functions.R")
-#       dataset=generateBivDist_withCov(n=n,a=a,b=b,c=c,mu1=mu1,mu2=mu2,dist=dist,x1=x1,x2=x2)
-#       #Set dataset as global as old S3 functions in gamlss and sometimes GJRM break without it
-#       assign("dataset", dataset, envir = .GlobalEnv)
-#       fits=fitBivModels_Bt_withCov(  dataset=dataset,      dist=dist,      include = "ALL",      a=a,b=b,c=c,mu1=mu1,mu2=mu2,      calc_actuals = FALSE,      cv = FALSE    )
-#       eval=evaluateModels(fits,vg_sims=100)
-#       return(eval)
-#     },
-#     args = list(dist = dist, a = a, b = b, c = c,
-#                 mu1 = mu1, mu2 = mu2, x1 = x1, x2 = x2, n = n)
-#   )
-#   eval_outer[[i]]=eval
-# }
-# 
-# score_items=list()
-# score_item_names=c("vs2_wt"   ,      "vs2",            "es","vs1"          ,  "vs2_wt_coronly","logliks")
-# for (i in 1:length(eval_outer)) {
-#   #item_names=names(eval_outer[[i]])
-#   for(item in score_item_names) {
-#     new_item=if(item=="logliks"){eval_outer[[i]][[item]][,1]}else{eval_outer[[i]][[item]]}
-#      
-#     if(i==1) {
-#       score_items[[item]]=new_item
-#     } else {
-#       score_items[[item]]=rbind(score_items[[item]],new_item)
-#     }
-#   }
-# }
-# 
-# par_estimates=list()
-# par_item_names=c("coefficients","ses","sigmas","correlations")
-# 
-# for(i in 1:11) {
-#   par_estimates[[i]]=matrix(nrow=length(eval_outer),ncol=16)
-# }
-# 
-# for(i in 1:length(eval_outer)) {
-#   z=1
-#   for(item in par_item_names) {
-#     new_item=data.frame(eval_outer[[i]][[item]])
-#     for (j in 1:ncol((new_item))) {
-#       par_estimates[[z]][i,]=new_item[,j]
-#       z=z+1; 
-#     }
-#   }
-# }
-# names(par_estimates)=c("mu1","mu2","x1","x2","mu1_se","mu2_se","x1_se","x2_se","sigma1","sigma2","corr")
-# for(item in names(par_estimates)) {
-#   colnames(par_estimates[[item]])=c("glm", "gee", "re_nosig", "re_np", "lme4", "gamm",
-#                                     "cop", "cop_n", "cop_j", "cop_g", "cop_f",
-#                                     "cop_amh", "cop_fgm", "cop_pl", "cop_h", "cop_t")
-# }
-# 
-# 
-# # Save the results
-# save(list = c("par_estimates", "score_items"), 
-#      file = paste0("Data/CoefSimData_", paste(dist, a, b, c, mu1, mu2, x1, x2, n, Sys.Date(), sep = "_"), ".RData"))
-# #save(list(par_estimates,score_items),file=paste("Data/CoefSimData_",paste(dist,a,b,c,mu1,mu2,x1,x2,n,Sys.Date(),sep="_"),".png",sep=""))
-# 
-# 
-
-#########AUTOMATED VERSION:
-
 # Load required library and source dependencies
 library(callr)
 source("common_functions.R")
 
 # Define the list of input parameter sets as a list of lists
 input_params_list <- list(
-#  list(dist="NO", a=1, b=1, c=0.25, mu1=1, mu2=2, x1=1, x2=0.1, n=1000),
-#  list(dist="NO", a=1, b=1, c=0.75, mu1=1, mu2=2, x1=1, x2=0.1, n=1000)
-  list(dist="GA", a=.5, b=2, c=0.25, mu1=1, mu2=2, x1=1, x2=0.1, n=1000),
-  list(dist="GA", a=1, b=1, c=0.25, mu1=1, mu2=2, x1=1, x2=0.1, n=1000),
-  list(dist="GA", a=2, b=.5, c=0.25, mu1=1, mu2=2, x1=1, x2=0.1, n=1000)
+  #list(dist="NO", a=1, b=1, c=0.25, mu1=1, mu2=2, x1=1, x2=0.1, n=1000),
+  #list(dist="NO", a=1, b=1, c=0.75, mu1=1, mu2=2, x1=1, x2=0.1, n=1000),
+  #list(dist="NO", a=1, b=1, c=0.9, mu1=1, mu2=2, x1=1, x2=0.1, n=1000)
+  #list(dist="GA", a=.5, b=2, c=NA, mu1=1, mu2=2, x1=1, x2=0.1, n=1000),
+  #list(dist="GA", a=1, b=1, c=NA, mu1=1, mu2=2, x1=1, x2=0.1, n=1000),
+  #list(dist="GA", a=2, b=.5, c=NA, mu1=1, mu2=2, x1=1, x2=0.1, n=1000),
+  list(dist="PO", a=NA, b=.2, c=5, mu1=.5, mu2=5, x1=1, x2=0.1, n=1000)
+  #list(dist="PO", a=NA, b=5, c=.2, mu1=.5, mu2=.5, x1=1, x2=0.1, n=1000),
+  #list(dist="PO", a=NA, b=.2, c=5, mu1=.5, mu2=5, x1=1, x2=0.1, n=1000),
+  #list(dist="PO", a=NA, b=5, c=.2, mu1=.5, mu2=.5, x1=1, x2=0.1, n=1000),
 )
 
 outer_sims <- 10  # Number of simulations per input set
