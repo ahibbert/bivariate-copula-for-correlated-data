1b74c7539f4211035a042ef3a19d59eaa0f31e68
ahibbert <a.d.hibbert@gmail.com>
2025-08-22 16:02:27 +1000

    Change chart shapes to be single row. Change log lik to show as -2 times log lik so we have all charts being lower the better.

--- TEMP Plotting for Covariate Output with Variogram Scores.R
+++ TEMP Plotting for Covariate Output with Variogram Scores.R
@@ -35,8 +35,8 @@ if (selection_method == "1") {
   cat("Using manual specification...\n")
   input_datasets <- list(
     list(
-      rdata= "Data/CoefSimData_NO_1_1_0.25_1_2_1_0.01_1000_10_2025-08-21.RData",
-      meta=list(dist="NO", a=1, b=1, c=0.25, mu1=1, mu2=2, x1=1, x2=0.01, n=1000)
+      rdata= "Data/CoefSimData_PO_NA_0.2_5_5_5_1_0.01_100_10_2025-08-22.RData",
+      meta=list(dist="PO", a=NA, b=.2, c=5, mu1=5, mu2=5, x1=1, x2=0.01, n=100)
     )
   )
 }
@@ -72,8 +72,8 @@ score_type_map <- c(
   vs1 = "Variogram Score (p=1)",
   vs2 = "Variogram Score (p=2)",
   vs2_wt = "Variogram Score (p=2, Weighted)",
-  vs2_wt_coronly = "Variogram Score (p=2, Correlated Obs Only)",
-  logliks = "Log Likelihood"
+  vs2_wt_coronly = "Variogram Score (p=2, Cor Only)",
+  logliks = "-2*Log Likelihood"
 )
 get_true_ses <- function(n, a, b, c, mu1, mu2, dist, x1, x2) {
   # Create a unique cache key from all parameters
@@ -116,7 +116,7 @@ for (ds in input_datasets) {
   for (i in 1:ncol(times)) {
     times_summary[i, "mean"] <- mean(times[, i], na.rm = TRUE)
     times_summary[i, "sd"] <- sd(times[, i], na.rm = TRUE)
-    times_summary[i, "conv"] <- conv[i]
+    times_summary[i, "conv"] <- mean(conv[,i])
   }
 
   rownames(times_summary) <- colnames(times)
@@ -132,7 +132,7 @@ for (ds in input_datasets) {
   # --- Score boxplots ---
   # Exclude logliks models not in other matrices
   ref_models <- colnames(score_items[[which(names(score_items) != "logliks")[1]]])
-  score_items$logliks <- score_items$logliks[, ref_models, drop = FALSE]
+  score_items$logliks <- -2*score_items$logliks[, ref_models, drop = FALSE]
 
   score_long <- map2_dfr(score_items, names(score_items), ~ {
     df <- as.data.frame(.x)
@@ -174,7 +174,7 @@ for (ds in input_datasets) {
     labs(title = paste("Model evaluation -", dist, "dist (a:", a, "b:", b, "c:", c, "μ1:", mu1, "μ2:", mu2, "x1:", x1, "x2:", x2, "n:", n, ")"),
          y = "Score Value", x = "Method") +
     theme(legend.position = "none",axis.text.x = element_text(angle = 45, hjust = 1))
-  print(p)
+  #print(p)
   ggsave(paste("Charts/Variogram_",paste(dist,a,b,c,mu1,mu2,x1,x2,n,Sys.Date(),sep="_"),".png",sep=""), plot = p, width = 9, height = 12, dpi = 900)
 
   # ---- Bottom 4 panels, outliers removed ----
@@ -199,10 +199,10 @@ for (ds in input_datasets) {
   p2 = ggplot(score_no_outliers, aes(x = method_label, y = value, fill = score_type_label)) +
     geom_boxplot(position = position_dodge(width = 0.8)) +
     geom_hline(data = meds_bottom4, aes(yintercept = min_median), linetype = "dashed", color = "red", inherit.aes = FALSE) +
-    facet_wrap(~score_type_label, scales = "free", ncol = 2, labeller = labeller(score_type_label = label_value)) +
+    facet_wrap(~score_type_label, scales = "free", ncol = 4, labeller = labeller(score_type_label = label_value)) +
     theme_bw() +
     labs(
-      title = paste("Model evaluation (Bottom Four Score Types, outliers removed) -", dist, "dist (a:", a, "b:", b, "c:", c, "μ1:", mu1, "μ2:", mu2, "x1:", x1, "x2:", x2, "n:", n, ")"),
+      #title = paste("Model evaluation (Bottom Four Score Types, outliers removed) -", dist, "dist (a:", a, "b:", b, "c:", c, "μ1:", mu1, "μ2:", mu2, "x1:", x1, "x2:", x2, "n:", n, ")"),
       y = "Score Value", x = "Method"
     ) +
     theme(
@@ -211,7 +211,7 @@ for (ds in input_datasets) {
     )
 
   print(p2)
-  ggsave(paste("Charts/Variogram_p2only_",paste(dist,a,b,c,mu1,mu2,x1,x2,n,Sys.Date(),sep="_"),"_bottom4_no_outliers.png",sep=""), plot = p2, width = 9, height = 9, dpi = 900)
+  ggsave(paste("Charts/Variogram_p2only_",paste(dist,a,b,c,mu1,mu2,x1,x2,n,Sys.Date(),sep="_"),"_bottom4_no_outliers.png",sep=""), plot = p2, width = 10, height = 3, dpi = 900)
 
   # ---- Coefficient plots: all coefficients ----
   results_list=par_estimates
@@ -258,7 +258,7 @@ for (ds in input_datasets) {
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
     labs(title = paste("Comparison of Models for x1, x2 -", dist, "dist (a:", a, "b:", b, "c:", c, "μ1:", mu1, "μ2:", mu2, "x1:", x1, "x2:", x2, "n:", n, ")"), y = "Value", x = "Model")
 
-  print(q)
+  #print(q)
   ggsave(plot=q,file=paste("Charts/AllCoefficients_",paste(dist,a,b,c,mu1,mu2,x1,x2,n,Sys.Date(),sep="_"),".png",sep=""), width = 12, height = 12, dpi = 900)
 
   # ---- Coefficient plots: only x1, x2, with true lines ----
@@ -301,9 +301,10 @@ for (ds in input_datasets) {
     left_join(coef_whisker_limits, by = c("variable", "model")) %>%
     filter(value >= lower & value <= upper)
 
+  #dist_name=if(dist=="PO") {"Negative Binomial"} else if (dist=="NO") {"Normal"} else if (dist=="GA") {"Gamma"} else {"Bernoulli"}
   coefplot=ggplot(results_long2_no_outliers, aes(x = model, y = value, fill = model)) +
     geom_boxplot(outlier.shape = NA) +
-    facet_wrap(~ variable, scales = "free", ncol = 2,
+    facet_wrap(~ variable, scales = "free", ncol = 4,
                labeller = as_labeller(facet_labels)) +
     geom_hline(
       data = true_vals_df,
@@ -313,26 +314,38 @@ for (ds in input_datasets) {
     theme_bw() +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
     labs(
-      title = paste("Coefficient Estimates and Standard Error for Each Model (10 Runs, outliers removed) -", dist, "dist (a:", a, "b:", b, "c:", c, "μ1:", mu1, "μ2:", mu2, "x1:", x1, "x2:", x2, "n:", n, ")"),
+      #title = paste(dist_name, ": X1, X2 Coef + SE (100 Simulations) -", "(a:", a, "b:", b, "c:", c, "μ1:", mu1, "μ2:", mu2, "n:", n, ")"),
       y = "Value",
       x = "Model"
     ) +
     theme(legend.position = "none")
   print(coefplot)
-  ggsave(plot=coefplot,file=paste("Charts/Coef_x1x2_only_",paste(dist,a,b,c,mu1,mu2,x1,x2,n,Sys.Date(),sep="_"),".png",sep=""), width = 9, height = 8, dpi = 900)
+  ggsave(plot=coefplot,file=paste("Charts/Coef_x1x2_only_",paste(dist,a,b,c,mu1,mu2,x1,x2,n,Sys.Date(),sep="_"),".png",sep=""), width = 10, height = 3, dpi = 900)
 }
 
+# ---- Initialize summary matrices for mean, sd, and convergence ----
 times_summary_avg <- matrix(nrow = length(times_summary_all), ncol = nrow(times_summary_all[[1]]))
-colnames(times_summary_avg)=rownames(times_summary_all[[1]])
+colnames(times_summary_avg) = rownames(times_summary_all[[1]])
 rownames(times_summary_avg) <- names(times_summary_all)
 
 times_summary_sd <- matrix(nrow = length(times_summary_all), ncol = nrow(times_summary_all[[1]]))
-colnames(times_summary_sd)=rownames(times_summary_all[[1]])
+colnames(times_summary_sd) = rownames(times_summary_all[[1]])
 rownames(times_summary_sd) <- names(times_summary_all)
+
+times_summary_conv <- matrix(nrow = length(times_summary_all), ncol = nrow(times_summary_all[[1]]))
+colnames(times_summary_conv) = rownames(times_summary_all[[1]])
+rownames(times_summary_conv) <- names(times_summary_all)
+
+times_summary_conv <- matrix(nrow = length(times_summary_all), ncol = nrow(times_summary_all[[1]]))
+colnames(times_summary_conv)=rownames(times_summary_all[[1]])
+rownames(times_summary_conv) <- names(times_summary_all)
 
 for (i in 1:length(times_summary_all)) {
   times_summary_avg[i,]=times_summary_all[[i]][, "mean"]
   times_summary_sd[i,]=times_summary_all[[i]][, "sd"]
+  times_summary_conv[i,]=times_summary_all[[i]][, "conv"]
 }
 
 print(times_summary_avg)
+print(times_summary_sd)
+print(times_summary_conv)
